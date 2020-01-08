b46e11b7a5ea4dcf5485c4e5a5d6db2843a014df
gunt <4261629+gunt@users.noreply.github.com>
2020-01-08 15:46:45 +0100

    update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,78 @@
 <template>
-  <div>
-    <input v-model="currentTodo" @keydown.enter="addTodo()" placeholder="Add a todo">
-    <ul class="todos">
-      <li v-for="todo in todos" :key="todo.id">
-        {{ todo.label }}
-      </li>
-    </ul>
-  </div>
+  <md-app>
+    <md-app-toolbar class="md-primary">
+      <span class="md-title">Todo App</span>
+    </md-app-toolbar>
+    <md-app-content>
+      <md-list class="todos">
+        <md-list-item>
+          <md-field>
+            <md-input v-model="currentTodo" @keydown.enter="addTodo()" placeholder="Add a todo"></md-input>
+          </md-field>
+        </md-list-item>
+        <span v-for="todo in todos" :key="todo.id">
+          <todoItem
+            :todo="todo"
+            @toggle-completed="toggleTodo(todo)"
+            @remove="removeTodo(todo)"
+            @toggle-editing="toggleEditing(todo)"
+            @finish-editing="pushEditChanges"
+          ></todoItem>
+        </span>
+      </md-list>
+    </md-app-content>
+  </md-app>
 </template>
 
 <script>
+import todoItem from "./components/todo-item";
 export default {
+  name: "app",
+  components: {
+    todoItem
+  },
   data() {
     return {
       todos: [],
-      currentTodo: ''
+      currentTodo: ""
     };
   },
   methods: {
     addTodo() {
-      this.todos.push({id: this.todos.length, label: this.currentTodo, completed: false});
-      this.currentTodo = '';
+      if (this.currentTodo) {
+        this.todos.push({
+          id: this.todos.length,
+          label: this.currentTodo,
+          completed: false,
+          isEditing: false
+        });
+        this.currentTodo = "";
+      }
+    },
+    removeTodo(todo) {
+      const index = this.todos.indexOf(todo);
+      this.todos.splice(index, 1);
+    },
+    toggleEditing(todo) {
+      const index = this.todos.indexOf(todo);
+      this.todos[index].isEditing = !this.todos[index].isEditing;
+    },
+    pushEditChanges(payload) {
+      const [todo, newLabel] = payload;
+      const index = this.todos.indexOf(todo);
+      this.todos[index].label = newLabel;
+      this.toggleEditing(todo);
+    },
+    toggleTodo(todo) {
+      const index = this.todos.indexOf(todo);
+      this.todos[index].completed = !this.todos[index].completed;
     }
   }
 };
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.isComplete {
+  text-decoration: line-through;
 }
 </style>
